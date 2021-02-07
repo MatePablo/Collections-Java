9236293c6b42b61b0eab4ee6d6522a9b6ccc9867
PabloStorino <pabloeralta@hotmail.com>
2021-02-07 18:26:49 -0300

    Api Stream: Profundidad, mejoras

--- Collections/src/main/java/ar/educacionit/tests/ApiStream.java
+++ Collections/src/main/java/ar/educacionit/tests/ApiStream.java
@@ -3,44 +3,74 @@ package ar.educacionit.tests;
 import ar.educacionit.entities.Persona;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class ApiStream {
 
     public static void main(String[] args) {
         
-        List<Persona> personas = new ArrayList();
+        /*
         
-        personas.add(new Persona("Pablo", "Storino", 24, "31313131311"));
-        personas.add(new Persona("Jorge", "Borges", 30, "42131231231"));
-        personas.add(new Persona("Marito", "Baracus", 30, "1111111111"));
+            Vease documentacion: Parte 09 - Collections y Generics.pdf
         
-        
-        //Api Stream
-        
-        // SELECT * FROM personas
-        personas.stream().forEach(System.out::println);
+        */
         
         
-        //SELECT * FROM personas WHERE edad = 30;        
-        // Sin un api stream:
-        System.out.println("\nSin api stream:");
+        /*
+            Api Stream:
         
-        for(Persona p:personas){
-            if(p.getEdad()==30){
-                System.out.println(p);
-            }
-        }
+            > La interfaz Stream representa una secuencia de elementos generica, es decir, del tipo de elementos que querramos.
         
-        // Con api stream:
-        System.out.println("\nCon api stream:");
+        
+        Tabla de "equivalencias" entre SQL y Stream:
+        
+            SQL         |           Stream
+        --------------------------------------------
+        FROM            |   stream()
+        SELECT          |   map()
+        WHERE           |   filter()
+        ORDER BY        |   sorted()
+        
+        */
+        
+        
+        //SELECT nombre FROM personas;
+
+        //Creo la lista
+        List<Persona> listaPersona = List.of(
+            new Persona("Pablo", "Storino", 24, "45454545"),
+            new Persona("Luis", "Pachionny", 31, "411111111"),
+            new Persona("Mariano", "Borges", 19, "8212341543"),
+            new Persona("Kevin", "Ramirez", 45, "214132561234")
+        );
+        
+        // La recorro en forma extensa
+        System.out.println("Forma extensa:");
+        Stream<String> stream = listaPersona.stream().map(p -> p.getNombre());
+        stream.forEach( p -> System.out.println(p) );
+        
+        // Forma verbosa / abreviada
+        System.out.println("\nForma abreviada:");
+        listaPersona
+                .stream()
+                .map(p -> p.getNombre())
+                .forEach(p -> System.out.println(p));
+        
+        // Con el metodo .stream() obtenemos una secuencia de elementos del tipo Persona. Este es el "FROM".
+        // Con el metodo .map() recuperamos solo el atributo "nombre". Este seria nuestro "SELECT".
+        // El punto clave es obtener un Stream mediante el metodo stream() y a partir de ahi ejecutar las acciones como filtrados, agrupaciones etc
+        
+        
+        // FILTRADO:
+        
+        // SELECT nombre FROM personas WHERE edad < 30
+        System.out.println("Lista de nombres de personas con edad menor a 30: ");
+        listaPersona
+                .stream()
+                .filter(p -> p.getEdad() < 30)
+                .map(p -> p.getNombre())
+                .forEach(p -> System.out.println(p));
                 
-        // El elemento filter crea una variable por cada elemento de la lista. Y luego evalua una expresion booleana, los elementos que den True seran filtrados a la nueva lista.
-        // .filter ( variable -> expresion booleana)
-        personas.stream()
-                .filter(p -> p.getEdad() == 30)
-                .forEach(System.out::println);
-        
-        
         
     }
 
